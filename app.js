a2c4a2ca824f02231f3eb6cd360e7ac4a4a85667
fatinFuyad <fuyad5266@gmail.com>
2025-05-06 12:50:19 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+const port = 8000;
+app.listen(port, () => {
+  console.log(`App is running on port:${port} 0__0`);
+});
