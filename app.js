9d597eb14113fc87267b4c27112daa06cde23bf1
fatinFuyad <fuyad5266@gmail.com>
2025-05-09 08:00:07 +0600

    include express router mounting

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const fs = require("fs");
 const express = require("express");
+
+// 1) MIDDLEWARES
+const morgan = require("morgan");
 const app = express();
+app.use(morgan("dev"));
 app.use(express.json());
+app.use((request, response, next) => {
+  console.log("Hello from the Middleware!🗃️");
+  next();
+});
+app.use((request, response, next) => {
+  request.requestTime = new Date().toISOString();
+  next();
+});
+
 // The express.json() function is a built-in middleware in Express that is used for parsing incoming requests with JSON payload and making that data available in the req.body or further processing within the routes. Without using express.json, Express will not automatically parse the JSON data in the request body.
 
 // app.get("/", (request, response) => {
@@ -14,16 +27,18 @@ app.use(express.json());
 // });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf-8"));
-app.get("/api/v1/tours", (request, response) => {
+
+// 2) ROUTE HANDLERS
+const getAllTours = (request, response) => {
   response.status(200).json({
     status: "success",
     results: tours.length,
     data: { tours: tours },
   });
-});
+};
 
-app.get("/api/v1/tours/:id", (request, response) => {
-  console.log(request.params);
+const getTour = (request, response) => {
+  // console.log(request.params);
   // // if a property has value of undefined then it's get removed in json
 
   const id = +request.params.id;
@@ -34,30 +49,26 @@ app.get("/api/v1/tours/:id", (request, response) => {
     return response.status(404).json({ status: "failed", messsage: "Invaild ID" });
   }
 
-  response.status(200).json({ status: "success", data: { tour } });
+  response
+    .status(200)
+    .json({ status: "success", requestedAt: request.requestTime, data: { tour } });
+};
 
-  // response.status(200).json({
-  //   status: "success",
-  //   results: tours.length,
-  //   data: { tours: tours },
-  // });
-});
-
-app.post("/api/v1/tours", (request, response) => {
+const createTour = (request, response) => {
   // console.log(request.body);
   // response.send("Post action has been completed!");
 
   const newTourId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newTourId }, request.body);
   tours.push(newTour);
-  console.log(newTour);
+  // console.log(newTour);
 
   fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (error) => {
     response.status(200).json({ status: "success", data: newTour });
   });
-});
+};
 
-app.patch("/api/v1/tours/:id", (request, response) => {
+const updatedTour = (request, response) => {
   const id = parseInt(request.params.id);
   if (id > tours.length) {
     return response.status(404).json({ status: "failed", message: "Invaid ID" });
@@ -74,12 +85,12 @@ app.patch("/api/v1/tours/:id", (request, response) => {
   fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (error) => {
     response.status(200).json({ status: "success", data: { tour: tour } });
   });
-});
+};
 
-app.delete("/api/v1/tours/:id", (request, response) => {
+const deleteTour = (request, response) => {
   const id = parseInt(request.params.id);
   if (id > tours.length) {
-    return response.status(404).json({ status: "failed", message: "Invaid ID" });
+    return response.status(204).json({ status: "failed", message: "Invaid ID" });
   }
 
   const updatedTours = tours.filter((el) => el.id !== id);
@@ -90,8 +101,60 @@ app.delete("/api/v1/tours/:id", (request, response) => {
       response.status(204).json({ status: "success", data: null }); // delete: status is 204
     }
   );
-});
+};
 
+const getAllUsers = (request, response) => {
+  response.status(500).json({
+    status: "Internal Server Error!💥💥",
+    message: "This route has not been defined yet",
+  });
+};
+
+const getUser = (request, response) => {
+  response.status(500).json({
+    status: "Internal Server Error!💥💥",
+    message: "This route has not been defined yet",
+  });
+};
+const createUser = (request, response) => {
+  response.status(500).json({
+    status: "Internal Server Error!💥💥",
+    message: "This route has not been defined yet",
+  });
+};
+const updateUser = (request, response) => {
+  response.status(500).json({
+    status: "Internal Server Error!💥💥",
+    message: "This route has not been defined yet",
+  });
+};
+const deleteUser = (request, response) => {
+  response.status(500).json({
+    status: "Internal Server Error!💥💥",
+    message: "This route has not been defined yet",
+  });
+};
+
+// app.get("/api/v1/tours", getAllTours);
+// app.get("/api/v1/tours/:id", getTour);
+// app.post("/api/v1/tours", createTour);
+// app.patch("/api/v1/tours/:id", updatedTour)
+// app.delete("/api/v1/tours/:id", deleteTour);
+
+// ROUTING MIDDLEWARES
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+// 3) ROUTES
+tourRouter.route("/").get(getAllTours).post(createTour);
+tourRouter.route("/:id").get(getTour).patch(updatedTour).delete(deleteTour);
+userRouter.route("/").get(getAllUsers).post(createUser);
+userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+// 4) START SERVER
 const port = 8000;
 app.listen(port, () => {
   console.log(`App is running on port:${port} 0__0`);
