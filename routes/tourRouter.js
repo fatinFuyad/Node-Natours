540983d757fd2a775865898cb3002c2c8465c3e8
fatinFuyad <fuyad5266@gmail.com>
2025-08-01 15:07:04 +0600

    includes basic routing before Refactor MVC

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -9,6 +9,7 @@ const {
 } = require("./../controllers/tourController");
 
 const router = express.Router();
+// router.param("id", checkId); // checkId is a middleware that checks if the id is valid
 
 router.route("/").get(getAllTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
--- server.js
+++ server.js
@@ -1,4 +1,24 @@
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
 const app = require("./app");
+const mongoose = require("mongoose");
+
+const database = process.env.DATABASE.replace("<db_password>", process.env.DATABASE_PASSWORD);
+// console.log(process.env);
+// mongoose previous options
+//  {
+// useNewUrlParser: true,
+// useUnifiedTopology: true,
+// useCreateIndex: true,
+// useFindAndModify: false,
+// }
+mongoose.connect(database).then((connectionObj) => {
+  // console.log(connectionObj.connections);
+  console.log("Database connection is successfull");
+});
+
 const port = 8000;
 app.listen(port, () => {
   console.log(`App is running on port:${port} 0__0`);
